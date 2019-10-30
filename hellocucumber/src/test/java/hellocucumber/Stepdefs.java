3eb0aef43b42fc43949f1f063014ae6223667152
Gabriel Balzer <45481960+GabrielBalzer@users.noreply.github.com>
2019-10-30 14:09:09 +0100

    Test Library Cucumber with Selenium Initial

---
+++ hellocucumber/src/test/java/hellocucumber/Stepdefs.java
@@ -0,0 +1,21 @@
+package hellocucumber;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.*;
+
+public class Stepdefs {
+
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        System.out.println("Do something");
+        ChromeDriver driver = new ChromeDriver();
+        String baseUrl = "https://www.google.com";
+        driver.get(baseUrl);
+        //throw new cucumber.api.PendingException();
+    }
+
+}
