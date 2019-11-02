723b826949e459b9a874e9a29964f8e3ea2d40ad
GabrielBalzer <gbalzer96@gmail.com>
2019-11-02 16:17:17 +0100

    Add Screenshots

--- hellocucumber/src/test/java/hellocucumber/Stepdefs.java
+++ hellocucumber/src/test/java/hellocucumber/Stepdefs.java
@@ -6,13 +6,7 @@ import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.*;
 
 public class Stepdefs {
 
