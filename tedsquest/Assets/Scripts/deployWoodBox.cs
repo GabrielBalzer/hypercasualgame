b1df3b2007748a4b83dd820b503d6e2c8ad8c2d3
michaela-gui <56273662+michaela-gui@users.noreply.github.com>
2020-05-10 22:39:59 +0200

    Added wood box and example to spawn obstacle.
    Not fully implemented yet.

---
+++ tedsquest/Assets/Scripts/deployWoodBox.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deployWoodBox : MonoBehaviour
+{
+    public GameObject woodboxPrefab;
+    public float respawnTime = 1.0f;
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        //StartCoroutine(Update());
+        Update();
+    }
+
+    private void spawnWoodBox()
+    {
+        //    GameObject a = Instantiate(woodboxPrefab) as GameObject;
+        //    a.transform.position = new Vector2(screenBounds.x * -2, Random.Range(-screenBounds.y, screenBounds.y));
+
+        //GameObject a = Instantiate(woodboxPrefab) as GameObject;
+        GameObject a = Instantiate<GameObject>(woodboxPrefab);
+        a.transform.position = transform.position + Camera.main.transform.forward * 2;
+    }
+
+    //IEnumerator WoodBoxWave()
+    void Update()
+    {
+        while (true)
+        {
+            //yield return new WaitForSeconds(respawnTime);
+            spawnWoodBox();
+        }
+    }
+}
---
+++ tedsquest/Assets/Scripts/rig.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rig : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [SerializeField]
+    //private bool moving;
+    private Vector3 velocity;
+
+    private void FixedUpdate()
+    {
+        transform.position += (velocity * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        TedPlayer ted = new TedPlayer();
+        if(ted != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Platform01 platform01 = collision.collider.GetComponent<Platform01>();
+        if(platform01 != null)
+        {
+            return;
+        }
+
+        if(collision.contacts[0].normal.y < -0.5)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
