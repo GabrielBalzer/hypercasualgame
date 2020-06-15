579c83ad5d163b9c534155738ad52051ba2816af
Daniel Wulfert <bionicledaniel@t-online.de>
2020-06-15 12:02:28 +0200

    Added Timer for the End of Game

---
+++ tedsquest/Assets/Scripts/EndSession.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndSession : MonoBehaviour
+{
+    public float delay;
+    bool loadingStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+
+
+
+    IEnumerator LoadLevelAfterDelay(float delayed)
+    {
+        delay = delayed;
+        loadingStarted = true;
+        do
+        {
+            yield return new WaitForSeconds(1);
+            //--delay;
+        }
+        while (--delay > 0);
+
+        SceneManager.LoadScene("StartScene");
+    }
+
+    private void OnGUI()
+    {
+        if (loadingStarted)
+        {
+            GUIStyle style = new GUIStyle(); 
+            style.fontSize = 30;
+            style.alignment = TextAnchor.MiddleCenter; 
+            GUI.Label(new Rect(Screen.width/2, 0, 100, 100), "Remaining Time: "+delay.ToString(), style);
+        }
+    }
+}
